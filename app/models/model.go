b053f3039d585916d391fb89e6108f356c81f790
nastya <120595306+DeaPacis@users.noreply.github.com>
2025-02-28 02:00:30 +0300

    chore(app): correct models

--- app/models/model.go
+++ app/models/model.go
@@ -5,12 +5,12 @@ type Song struct {
 	Group       string `json:"group"`
 	Song        string `json:"song"`
 	ReleaseDate string `json:"releaseDate,omitempty"`
-	Lyrics      string `json:"lyrics,omitempty"`
+	Text        string `json:"text,omitempty"`
 	Link        string `json:"link,omitempty"`
 }
 
 type SongDetail struct {
 	ReleaseDate string `json:"releaseDate"`
-	Lyrics      string `json:"lyrics"`
+	Text        string `json:"text"`
 	Link        string `json:"link"`
 }
